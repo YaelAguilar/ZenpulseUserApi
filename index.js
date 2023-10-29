642e1c3d02502e47536671cfc958bbed7b4ed50c
YaelAguilar <213452@ids.upchiapas.edu.mx>
2023-10-29 07:31:47 -0600

    change port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app')
-const {PORT} = process.env
-app.listen(PORT, () => {
-    console.log(`SERVER is running at port: ${PORT}`);
+//const {PORT} = process.env
+app.listen(3000, () => {
+    console.log(`SERVER is running at port: ${3000}`);
 })
