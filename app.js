8feb2dd18c7990ea7ff7aaf0d5f75a6a7ef13aa9
YaelAguilar <213452@ids.upchiapas.edu.mx>
2023-10-29 19:01:14 -0600

    login create

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const User = require('./model/user')
 const express = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
+
 
 app.get("/", (req, res) => {
     res.send("<h1>Server is working</h1>")
@@ -46,13 +49,57 @@ app.post("/register", async (req, res) => {
         user.token = token
         user.password = undefined
 
-            res.status(201).json(user)
+
+        res.status(201).json(user)
 
 
     } catch (error) {
         console.log(error);
     }
 })
+
+app.post('/login', async (req, res) => {
+    try {
+        //get all data from frontend
+        const {email, password} = req.body
+        //validation
+        if (!(email && password)) {
+            res.status(400).send('send all data')
+        }
+        //find user in DB
+        const user = await User.findOne({email})
+        //assignmennt if user is not there, then what ?
+
+        //match the password
+        if (user && (await bcrypt.compare(password, user.password))) {
+            const token = jwt.sign(
+                {id: user._id},
+                'shhhh', //process.env.jwtsecret
+                {
+                    expiresIn: "1h"
+                }
+            );
+            user.token = token
+            user.password = undefined
+
+            //cookie section
+            const options = {
+                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly: true
+            };
+            res.status(200).cookie("token", token, options).json({
+                success: true,
+                token,
+                user
+            })
+        }
+
+        
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 
 
 
