3bb9670fbe34bb6450e9cb8e544957dc351d2dbe
YaelAguilar <213452@ids.upchiapas.edu.mx>
2023-11-10 22:59:23 -0600

    [ADD] security

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-require('dotenv').config()
-require("./database/database").connect()
-const User = require('./model/user')
-const express = require('express')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const auth = require('./middleware/auth')
+// app.js
+require('dotenv').config();
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const auth = require('./middleware/auth');
+const User = require('./model/user');
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
-
-app.use(cors())
-
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: process.env.CORS_ALLOWED_ORIGINS || 'http://localhost:3000' }));
 
 app.get("/", (req, res) => {
-    res.send("<h1>Server is working</h1>")
-})
+    res.send("<h1>Server is working</h1>");
+});
 
 app.post("/register", async (req, res) => {
     try {
@@ -26,15 +24,13 @@ app.post("/register", async (req, res) => {
 
         // Validar que todos los campos sean obligatorios
         if (!(firstname && lastname && email && password)) {
-            //return res.status(400).json({ error: 'Todos los campos son obligatorios' });
             return res.status(400).send('Todos los campos son obligatorios');
         }
 
         // Verificar si el usuario ya existe - email
         const existingUser = await User.findOne({ email });
         if (existingUser) {
-            return res.status(401).send('Ya existe un usuario con este correo electrónico');
-            //return res.status(400).send('Ya existe un usuario con este correo electrónico');
+            return res.status(401).json({ error: 'Ya existe un usuario con este correo electrónico' });
         }
 
         // Encriptar la contraseña
@@ -51,9 +47,9 @@ app.post("/register", async (req, res) => {
         // Generar un token para el usuario y enviarlo
         const token = jwt.sign(
             { id: user._id, email },
-            'shhhh', // Deberías usar process.env.jwtsecret en producción
+            process.env.JWT_SECRET || 'defaultSecret',
             {
-                expiresIn: "1h"
+                expiresIn: process.env.COOKIE_EXPIRATION || '1h'
             }
         );
         user.token = token;
@@ -62,7 +58,7 @@ app.post("/register", async (req, res) => {
         res.status(201).json(user);
     } catch (error) {
         console.log(error);
-        res.status(500).send('Error interno del servidor: ${error.message}');
+        res.status(500).send(`Error interno del servidor: ${error.message}`);
     }
 });
 
@@ -87,9 +83,9 @@ app.post('/login', async (req, res) => {
         // Generar un token y configurar las cookies
         const token = jwt.sign(
             { id: user._id },
-            'shhhh', // Deberías usar process.env.jwtsecret en producción
+            process.env.JWT_SECRET || 'defaultSecret',
             {
-                expiresIn: '1h'
+                expiresIn: process.env.COOKIE_EXPIRATION || '1h'
             }
         );
 
@@ -97,7 +93,7 @@ app.post('/login', async (req, res) => {
         user.password = undefined;
 
         const options = {
-            expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+            expires: new Date(Date.now() + (process.env.COOKIE_EXPIRATION || 24 * 60 * 60 * 1000)),
             httpOnly: true
         };
 
@@ -113,12 +109,15 @@ app.post('/login', async (req, res) => {
     }
 });
 
-
-app.get("/dashbo",auth, (req, res) => {
-
+app.get("/dashbo", auth, (req, res) => {
     console.log(req.user);
-    res.send('Welcome to dashboard')
-})
+    res.send('Welcome to dashboard');
+});
 
+const port = process.env.PORT || 3000;
 
-module.exports = app
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+module.exports = app;
