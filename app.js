90a206ff6a68a90569313cba512457fd8470f139
YaelAguilar <213452@ids.upchiapas.edu.mx>
2023-11-11 00:40:27 -0600

    [UPDATE] reload old code

--- app.js
+++ app.js
@@ -1,123 +1,117 @@
-// app.js
-require('dotenv').config();
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const auth = require('./middleware/auth');
-const User = require('./model/user');
+require('dotenv').config()
+require("./database/database").connect()
+const User = require('./model/user')
+const express = require('express')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const auth = require('./middleware/auth')
 
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({ origin: process.env.CORS_ALLOWED_ORIGINS || 'http://localhost:3000' }));
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
 
 app.get("/", (req, res) => {
-    res.send("<h1>Server is working</h1>");
-});
+    res.send("<h1>Server is working</h1>")
+})
 
 app.post("/register", async (req, res) => {
     try {
-        // Obtener todos los datos del cuerpo
-        const { firstname, lastname, email, password } = req.body;
-
-        // Validar que todos los campos sean obligatorios
-        if (!(firstname && lastname && email && password)) {
-            return res.status(400).send('Todos los campos son obligatorios');
+        //get all data from body
+        const {firstname, lastname, email, password} = req.body 
+        //all the data should exists
+        if (!(firstname && lastname && email && password)){
+            res.status(400).send('All fields are compulsory')
+        }
+        //check if user already exists - email
+        const existingUser = await User.findOne({ email })
+        if (existingUser){
+            return res.status(401).send('User already exists with tihs email')
         }
 
-        // Verificar si el usuario ya existe - email
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(401).json({ error: 'Ya existe un usuario con este correo electrónico' });
-        }
-
-        // Encriptar la contraseña
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Guardar el usuario en la base de datos
+        //encrypt the password
+        const myEncpassword = await bcrypt.hash(password, 10)
+        //save the user in DB
         const user = await User.create({
             firstname,
             lastname,
             email,
-            password: hashedPassword
-        });
-
-        // Generar un token para el usuario y enviarlo
+            password: myEncpassword
+        })
+        //generate a token for user and send it
         const token = jwt.sign(
-            { id: user._id, email },
-            process.env.JWT_SECRET || 'defaultSecret',
+            {id: user._id, email},
+            'shhhh', //process.env.jwtsecret
             {
-                expiresIn: process.env.COOKIE_EXPIRATION || '1h'
+                expiresIn: "1h"
             }
         );
-        user.token = token;
-        user.password = undefined;
+        user.token = token
+        user.password = undefined
 
-        res.status(201).json(user);
+
+        res.status(201).json(user)
+
+
+
     } catch (error) {
         console.log(error);
-        res.status(500).send(`Error interno del servidor: ${error.message}`);
+        res.status(500).send('Internal Server Error');
     }
-});
+})
 
 app.post('/login', async (req, res) => {
     try {
         // Obtener todos los datos del frontend
         const { email, password } = req.body;
-
+        
         // Validación
         if (!(email && password)) {
             return res.status(400).send('Envía todos los datos necesarios');
         }
-
+        
         // Buscar al usuario en la base de datos
         const user = await User.findOne({ email });
-
-        // Verificar si el usuario no existe o las credenciales son inválidas
+        
+        // Verificar si el usuario no existe
         if (!user || !(await bcrypt.compare(password, user.password))) {
             return res.status(401).send('Credenciales inválidas');
         }
 
-        // Generar un token y configurar las cookies
         const token = jwt.sign(
             { id: user._id },
-            process.env.JWT_SECRET || 'defaultSecret',
+            'shhhh', // Deberías usar process.env.jwtsecret en producción
             {
-                expiresIn: process.env.COOKIE_EXPIRATION || '1h'
+                expiresIn: '1h'
             }
         );
-
+        
         user.token = token;
         user.password = undefined;
 
         const options = {
-            expires: new Date(Date.now() + (process.env.COOKIE_EXPIRATION || 24 * 60 * 60 * 1000)),
+            expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
             httpOnly: true
         };
 
-        res.cookie('token', token, options);
-        res.status(200).json({
+        return res.status(200).cookie('token', token, options).json({
             success: true,
             token,
             user
         });
     } catch (error) {
         console.log(error);
-        res.status(500).send('Error interno del servidor');
+        return res.status(500).send('Error interno del servidor');
     }
 });
 
-app.get("/dashbo", auth, (req, res) => {
+
+app.get("/dashboard",auth, (req, res) => {
+
     console.log(req.user);
-    res.send('Welcome to dashboard');
-});
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
-
-module.exports = app;
+    res.send('Welcome to dashboard')
+})
+module.exports = app
